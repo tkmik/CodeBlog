fa1e78ca98f953ada4bcf1744684289ab22ab0e6
tkmik <tkach_nikita@outlook.com>
2021-04-20 13:21:14 +0300

    added a new project working with GC

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"memory {GC.GetTotalMemory(false)}");
+            for (int i = 0; i < 100000; i++)
+            {
+                var obj = (object)i;
+                int j = (int)obj;
+            }
+                        
+            Console.WriteLine($"memory {GC.GetTotalMemory(false)}");
+            GC.Collect(1);
+            Console.WriteLine($"memory {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"count of GC.Collect(1st generation) {GC.CollectionCount(1)}");
+            Console.WriteLine($"count of GC.Collect(2nd generation) {GC.CollectionCount(2)}");
+            Console.WriteLine($"max generation {GC.MaxGeneration}");
+            Console.WriteLine($"total allocated bytes {GC.GetTotalAllocatedBytes()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GarbageCollector/TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector
+{
+    class TestClass : IDisposable
+    {
+        private bool disposed = false;
+        public TestClass()
+        {
+
+        }
+
+        ~TestClass()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // release managed resources
+                }
+                // release unmanaged resources
+                disposed = true;
+            }
+        }
+    }
+}
